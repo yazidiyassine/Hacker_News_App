1d8d735432232c2a3ebe181047047d70d1c21b76
unknown <yassine.yzd@gmail.com>
2023-01-23 21:46:17 +0100

    App's lifecycle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,80 @@
 import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  const list = [
+const list = [
+  {
+    title: "React",
+    url: "https://facebook.github.io/react/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://github.com/reactjs/redux",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+    onDismiss(id){
+      const isNotId = item => item.objectID !== id;
+      const updatedList = this.state.list.filter(isNotId);
+      this.setState({ list: updatedList})
+    }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.list.map((item) => (
+          <div key={item.objectID}>
+            <span>
+              <a href="{item.url}">{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={()=>this.onDismiss(item.objectID)} type="button">
+                  Dismiss
+                </button>
+            </span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+/* function App() {
+  class Developper{
+    constructor(firstname, lastname){
+      this.firstname = firstname;
+      this.lastname = lastname;
+    }
+    getName(){
+      return this.firstname + ' ' + this.lastname;
+    }
+  }
+  const dev = new Developper("Yassine", "Yazidi");
+  return(
+    console.log("Hi, ",dev.getName())
+  );
+
+} */
+/* const list = [
     {
       title: "React",
       url: "https://facebook.github.io/react/",
@@ -18,12 +91,12 @@ function App() {
       points: 5,
       objectID: 1,
     },
-  ];
-  return (
+  ]; */
+/*  return (
     <div className="App">
-      {/* {list.map(function (item) { */}
-      {/* Using the arrow function */}
-      {list.map((item) => (
+      {/* {list.map(function (item) { }*/
+/* Using the arrow function */
+/* {list.map((item) => (
         <div key={item.objectID}>
           <span>
             <a href={item.url}>{item.title}</a>
@@ -34,9 +107,9 @@ function App() {
         </div>
       ))}
     </div>
-  );
+  ); */
 
-  /*  const welcome = "Welcome to the new react App";
+/*  const welcome = "Welcome to the new react App";
   function greetUser(user) {
     if (user) {
       return <h3>Hello, {formatUser(user)}</h3>;
@@ -54,6 +127,5 @@ function App() {
       (new Date().getFullYear() - user.bd)
     );
   } */
-}
 
 export default App;

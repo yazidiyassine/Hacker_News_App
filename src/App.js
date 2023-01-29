8e3ac11065cc9b6be2d2326d1a3ad61a8d501f9d
yazidiyassine <yassine.yzd@gmail.com>
2023-01-29 16:10:20 +0100

    Code Playground v1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import "./App.css";
 import React, { Component } from "react";
 
+// Binding
+/*class ExplainBindingComponents extends Component {
+  // ==> Binding using the bind() inside the constructor
+  /* constructor(){
+    super();
+    this.OnClickMe = this.OnClickMe.bind(this)
+  }
+  OnClickMe() {
+    console.log(this);
+  } */
+
+// ==> Auto-binding using the Arrow Function
+/*onClickMe = () => {
+    console.log(this);
+  };
+  render() {
+    return (
+      <button onClick={this.onClickMe} type="Button">
+        Click Me!
+      </button>
+    );
+  }
+}*/
+
 const list = [
   {
     title: "React",
@@ -24,15 +48,44 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: list
+      list: list,
     };
     this.onDismiss = this.onDismiss.bind(this);
   }
-    onDismiss(id){
-      const isNotId = item => item.objectID !== id;
-      const updatedList = this.state.list.filter(isNotId);
-      this.setState({ list: updatedList})
-    }
+  onDismiss(id) {
+    const isNotId = (item) => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  // Event Handlers
+  /*render() {
+    return (
+      <div className="App">
+        {this.state.list.map((item) => {
+          const onDismissHandler = () => this.onDismiss(item.objectID);
+
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href="{item.url}">{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                {/* <button onClick={onDismissHandler} type="button"> }
+                /*<button onClick={ () => 
+                  console.log(item.objectID)} type="button">
+                  Dismiss
+                </button>
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }*/
 
   render() {
     return (
@@ -47,9 +100,11 @@ class App extends Component {
             <span>{item.points}</span>
             <span>
               <button
-                onClick={()=>this.onDismiss(item.objectID)} type="button">
-                  Dismiss
-                </button>
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
             </span>
           </div>
         ))}
@@ -57,7 +112,6 @@ class App extends Component {
     );
   }
 }
-
 /* function App() {
   class Developper{
     constructor(firstname, lastname){
@@ -74,24 +128,7 @@ class App extends Component {
   );
 
 } */
-/* const list = [
-    {
-      title: "React",
-      url: "https://facebook.github.io/react/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://github.com/reactjs/redux",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ]; */
+
 /*  return (
     <div className="App">
       {/* {list.map(function (item) { }*/
@@ -128,4 +165,5 @@ class App extends Component {
     );
   } */
 
+//export default ExplainBindingComponents;
 export default App;

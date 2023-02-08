bf1c9730dc405b8a6499d45ef508a712d1194db5
yazidiyassine <yassine.yzd@gmail.com>
2023-02-08 20:47:56 +0100

    Code PlayGround: Iterations with Forms and Events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,84 @@
 import "./App.css";
 import React, { Component } from "react";
 
+
+const list = [
+  {
+    title: "React",
+    url: "https://facebook.github.io/react/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://github.com/reactjs/redux",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+// ==> Concept of the H.O.F (H.O.C)
+function isSearched(searchTerm) {
+  return function (item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list,
+      searchTerm: "",
+    };
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+  onDismiss(id) {
+    const isNotId = (item) => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  // ==> Iterations with Forms and Events
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <input onChange={this.onSearchChange} type="text" />
+        </form>
+        {this.state.list
+          .filter(isSearched(this.state.searchTerm))
+          .map((item) => (
+            <div key={item.objectID}>
+              <span>
+                <a href="{item.url}">{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type="button"
+                >
+                  Dismiss
+                </button>
+              </span>
+            </div>
+          ))}
+      </div>
+    );
+  }
+}
+
 // Binding
 /*class ExplainBindingComponents extends Component {
   // ==> Binding using the bind() inside the constructor
@@ -25,41 +103,8 @@ import React, { Component } from "react";
   }
 }*/
 
-const list = [
-  {
-    title: "React",
-    url: "https://facebook.github.io/react/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://github.com/reactjs/redux",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: list,
-    };
-    this.onDismiss = this.onDismiss.bind(this);
-  }
-  onDismiss(id) {
-    const isNotId = (item) => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList });
-  }
-
-  // Event Handlers
-  /*render() {
+// Event Handlers
+/*render() {
     return (
       <div className="App">
         {this.state.list.map((item) => {
@@ -87,31 +132,6 @@ class App extends Component {
     );
   }*/
 
-  render() {
-    return (
-      <div className="App">
-        {this.state.list.map((item) => (
-          <div key={item.objectID}>
-            <span>
-              <a href="{item.url}">{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button"
-              >
-                Dismiss
-              </button>
-            </span>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
 /* function App() {
   class Developper{
     constructor(firstname, lastname){
